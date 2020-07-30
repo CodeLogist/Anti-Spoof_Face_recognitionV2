ca2738f3d07442ffca92e76002ea24b26da39517
CodeLogist <mayanktale9425@gmail.com>
2020-07-30 21:31:28 +0530

    Update

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ import os
 from src.anti_spoof_predict import AntiSpoofPredict
 from src.generate_patches import CropImage
 from src.utility import parse_model_name
-# warnings.filterwarnings('ignore')
 
 print("[File]: ",torch.__file__)
 print("[Version]: ",torch.__version__)
